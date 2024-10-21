3f7f57c491f2e314282832acce6a9325a335bbc2
unknown <umayerpro175@gmail.com>
2024-10-21 10:04:21 +0600

    MAE server

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Manage asset explorer (MAE) sever is  ready ");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.34gmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("MAE").collection("users");
+    // user api
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/users-role", async (req, res) => {
+      const query = req.query.email;
+
+      const result = await userCollection.find({ email: query }).toArray();
+      res.send(result);
+    });
+
+    app.post("/register-hr", async (req, res) => {
+      const { name, email, password, dob, companyName, logo, memberPackage } =
+        req.body;
+
+      const hr = {
+        name,
+        email,
+        password,
+        dob,
+        companyName,
+        logo,
+        memberPackage,
+        role: "hr",
+      };
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user) {
+        return res.send({ message: "user already exists", insertId: null });
+      }
+
+      const result = await userCollection.insertOne(hr);
+      res.send(result);
+    });
+
+    //update hr data  after payment
+    app.patch("/register-hr", async (req, res) => {
+      const { paymentStatus, transactionId, hrId } = req.body;
+      const filter = { _id: new ObjectId(hrId) };
+      const updatedDoc = {
+        $set: {
+          paymentStatus: paymentStatus,
+          transactionId: transactionId,
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // hr payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { memberPackage } = req.body;
+      const amount = parseInt(memberPackage * 100);
+
+      // create a paymentIntent with the select package amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`This Manage asset explorer (MAE) server running PORT:${port}`);
+});
+
+
+
